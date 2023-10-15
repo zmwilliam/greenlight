6ce1bc89bde48d9096c0fb1d6ad49516ad95064d
zmwilliam <willcxa@gmail.com>
2023-10-15 19:42:05 -0300

    feat(data): Runtime custom unmarshal

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +15,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	value := fmt.Sprintf("%d mins", r)
 	valueQuoted := strconv.Quote(value)
 	return []byte(valueQuoted), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
