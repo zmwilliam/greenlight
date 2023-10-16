398e81d955acced7d636864837fafa25083cc12d
zmwilliam <willcxa@gmail.com>
2023-10-15 21:06:57 -0300

    feat(api): Adding validator

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-func (app application) logError(r *http.Request, err error) {
+func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
 }
 
-func (app application) errorResponse(
+func (app *application) errorResponse(
 	w http.ResponseWriter,
 	r *http.Request,
 	status int,
@@ -24,18 +24,30 @@ func (app application) errorResponse(
 	}
 }
 
-func (app application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
-func (app application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-func (app application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resouce", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) failedValidationResponse(
+	w http.ResponseWriter,
+	r *http.Request,
+	errors map[string]string,
+) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/zmwilliam/greenlight/internal/data"
+	"github.com/zmwilliam/greenlight/internal/validator"
 )
 
 func (app application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +41,30 @@ func (app application) createMovieHandler(w http.ResponseWriter, r *http.Request
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be longer than 500 bytes")
+
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(input.Runtime != 0, "runtime", "must be provided")
+	v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
