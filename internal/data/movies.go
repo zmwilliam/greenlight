f53a439aae5fe26769130329c7b28b2fc58f6280
zmwilliam <willcxa@gmail.com>
2023-10-15 21:15:59 -0300

    refactor(api): Movie validator

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/zmwilliam/greenlight/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -10,4 +14,22 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime"`
 	Genres    []string  `json:"genres"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, m *Movie) {
+	v.Check(m.Title != "", "title", "must be provided")
+	v.Check(len(m.Title) <= 500, "title", "must not be longer than 500 bytes")
+
+	v.Check(m.Year != 0, "year", "must be provided")
+	v.Check(m.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(m.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(m.Runtime != 0, "runtime", "must be provided")
+	v.Check(m.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(m.Genres != nil, "genres", "must be provided")
+	v.Check(len(m.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(m.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	v.Check(validator.Unique(m.Genres), "genres", "must not contain duplicate values")
 }
