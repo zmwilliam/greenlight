9f07b743ebbc5e89b5a6be57a4af1c3445e3db98
zmwilliam <willcxa@gmail.com>
2023-10-19 08:57:06 -0300

    fix(migrations): adding title to movies

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS movies (
   id bigserial PRIMARY KEY,
   create_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
   title text NOT NULL,
+  year integer NOT NULL,
   runtime integer NOT NULL,
   genres text[] NOT NULL,
   version integer NOT NULL DEFAULT 1
