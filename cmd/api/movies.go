52e92c6ccb41674a47e738a15f90ebff383f4906
zmwilliam <willcxa@gmail.com>
2023-10-22 02:34:18 -0300

    feat(movies): fetching movies from database

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/zmwilliam/greenlight/internal/data"
 	"github.com/zmwilliam/greenlight/internal/validator"
@@ -16,14 +16,15 @@ func (app application) showMovieHandler(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		Title:     "The Shawnshank Redemption",
-		Runtime:   142,
-		Genres:    []string{"drama", "prision", "friendship"},
-		Year:      1994,
-		CreatedAt: time.Now(),
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/lib/pq"
@@ -42,7 +43,35 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	return nil, nil
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	query := ` 
+	SELECT id, created_at, title, year, runtime, genres, version
+	FROM movies
+	WHERE id = $1`
+
+	var movie Movie
+
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID,
+		&movie.CreatedAt,
+		&movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
